e776b8e4c713204a1f28440fd126664308d0e2b3
Kenny Krosky <kenny.krosky@gmail.com>
2021-10-07 09:26:19 -0700

    bump

---
+++ pages/article/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{post.name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  async asyncData({params}) {
+    const post = await fetch(`https://jsonplaceholder.typicode.com/users/${params.slug}`).then((res) => res.json())
+    return {post}
+  }
+}
+</script>
+
+<style>
+
+</style>
