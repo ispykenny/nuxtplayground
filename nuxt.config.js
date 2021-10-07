666d731aa27f0f3be642385d92f2fd4f1c8537c3
Kenny Krosky <kenny.krosky@gmail.com>
2021-10-06 19:28:39 -0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mynuxtsite',
+    title: 'Nuxt Playground',
     htmlAttrs: {
       lang: 'en'
     },
