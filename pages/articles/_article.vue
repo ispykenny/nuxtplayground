eaf40699620e57e04ed47aed28e5a05509d959f5
Kenny Krosky <kenny.krosky@gmail.com>
2021-10-07 19:53:02 -0700

    playground

---
+++ pages/articles/_article.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div><nuxt-link to="/">Go Back</nuxt-link></div>
+    <img :src="article.fields.featuredImage.fields.file.url" :alt="`${article.fields.title} preview`"/>
+    <h1>{{article.fields.title}}</h1>
+    <div v-html="thePost(article.fields.article)"></div>
+  </div>
+</template>
+
+<script>
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+export default {
+  async asyncData({params}) {
+    const data = await fetch(`https://contentful-endpoint.herokuapp.com/post/${params.article}`).then((res) => res.json())
+    console.log(data)
+    return {post:data}
+  }, 
+  head() {
+    return {
+      title: this.post.fields.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.post.fields.seoMeta
+        }
+      ]
+    }
+  },
+  computed: {
+    article() {
+      return this.post
+    }
+  },
+  methods: {
+    thePost(article) {
+      return documentToHtmlString(article)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,52 @@
 <template>
   <div>
-    <div v-for="person in allPosts" :key="person.id">
-      <NuxtLink :to="`/article/${person.id}`">
-        <h1>Name: {{person.name}}</h1>
-        <p>User Name:: {{person.username}}</p>
-        <hr/>
-      </NuxtLink>
-    </div>  
+    <div v-for="post in allPosts.items" :key="post.sys.id">
+      <nuxt-link :to="`/articles/${post.fields.title.toLowerCase().split(' ').join('-')}`">
+        <div>
+          <img :src="post.fields.featuredImage.fields.file.url" :alt="`${post.fields.title} preview`"/>
+          <h4>{{post.fields.title}}</h4>
+          <div v-html="htmlPost(post.fields.excerpt)"></div>
+        </div>
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
 <script>
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+
 export default {
+  data() {
+    return {
+      showPre: false
+    }
+  },
+  head() {
+    return {
+      title: "Articles",
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Home page description'
+        }
+      ]
+    }
+  },
   async asyncData() {
-    const data = await fetch('https://jsonplaceholder.typicode.com/users/').then((res) => res.json());
+    const data = await fetch('https://contentful-endpoint.herokuapp.com/articles').then((res) => res.json());
     return { data: data }
   },
   computed: {
     allPosts() {
       return this.data
-    }
+    },
+  },
+  methods: {
+    togglePre() {
+      this.showPre = !this.showPre
+    },
+    htmlPost : document => documentToHtmlString(document)
   }
 }
 </script>
